d54f4781f8212505d7f4524f99ef5a165c9598cc
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-01-13 09:21:27 +0530

    delete logic working

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,24 @@ export default function App() {
     console.log(todos);
     setNewItem("")
   }
+
+  function toggleTodo(id, completed){
+    setTodos( currentTodos => {
+      return currentTodos.map(todo => {
+        if(todo.id === id){
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -63,10 +81,10 @@ export default function App() {
           return (
             <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed}/>
+                <input type="checkbox" onChange={e => toggleTodo(todo.id, e.target.checked)} checked={todo.completed}/>
                 {todo.title}
               </label>
-              <button className="btn btn-danger">Delete</button>
+              <button className="btn btn-danger" onClick={() => deleteTodo(todo.id)}>Delete</button>
             </li>
           );
         })}

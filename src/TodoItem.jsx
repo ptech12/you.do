b3f0cbebe4c08964133c38abd533896c378bf20f
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-01-14 20:47:01 +0530

    Update

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -17,6 +17,7 @@ export default function TodoItem({
         />
         {title}
       </label>
+      
       <button className="btn btn-danger" onClick={() => deleteTodo(id)}>
         Delete
       </button>
